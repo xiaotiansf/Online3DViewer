d983776acc5b3d96f04b1dd99fa5a2c3c771b03f
xiaos <xiaos@xiaos-clx-desktop>
2024-01-05 15:02:30 -0800

    created export class ClientSocket

--- source/website/client.js
+++ source/website/client.js
@@ -1,74 +1,79 @@
 import net from 'net'
 const port = 9838;
 const host = '127.0.0.1';
-const timeout = 5000;
+const timeout = 2000;
 let retrying = false;
 
 // Functions to handle socket events
-function makeConnection () {
+function makeConnection (socket) {
    socket.connect(port, host);
 }
 
-function connectEventHandler() {
-   console.log('connected');
-   retrying = false;
-}
-
-function dataEventHandler() {
-   console.log(data.toString());
-   let cmd_string = data.toString();
-   let hashtag_index = cmd_string.indexOf('#{');
-   if (hashtag_index > 0) {
-      let json = cmd_string.slice(hashtag_index+1);
-      const obj = JSON.parse(json);
-      console.log(obj.filename);
-      console.log(obj.info);
-      console.log(obj.direction);
-      console.log(obj.device_orientation);
-      console.log(obj.display_mode);
-      console.log(obj.zoom);
-      console.log(obj.assetDir);
+export class ClientSocket
+{
+   constructor ()
+   {
+      this.socket = new net.Socket();
+      this.filename = null;
+      this.zoom = 1.0;
+      this.panX = 0.0;
+      this.panY = 0.0;
    }
-   else {
-      console.log('received invalid string from palacio-display-server');
+
+   MakeConnection = () =>
+   {
+      this.socket.connect(port, host);
+   }
+
+   Init()
+   {
+      this.socket.on("connect", () => {
+         console.log('connected');
+         retrying = false;
+      })
+      this.socket.on("data", data => {
+         console.log(data.toString());
+         let cmd_string = data.toString();
+         let hashtag_index = cmd_string.indexOf('#{');
+         if (hashtag_index > 0) {
+            let json = cmd_string.slice(hashtag_index+1);
+            const obj = JSON.parse(json);
+            console.log(obj.filename);
+            console.log(obj.info);
+            console.log(obj.direction);
+            console.log(obj.device_orientation);
+            console.log(obj.display_mode);
+            console.log(obj.zoom);
+            console.log(obj.assetDir);
+            this.filename = obj.filename;
+         }
+         else {
+            console.log('received invalid string from palacio-display-server');
+         }
+      })
+      this.socket.on("end", () => {
+         console.log("Connection ended");
+      })
+      this.socket.on("timeout", () => {
+         console.log('timeout');
+      })
+      this.socket.on("drain", () => {
+         console.log('drain');
+      })
+      this.socket.on("error", () => {
+         console.log('error');
+      })
+      this.socket.on("close", () => {
+         console.log('close');
+         if (!retrying) {
+            retrying = true;
+            console.log('Reconnecting...');
+         }
+         setTimeout(this.MakeConnection, timeout);
+      })
+
+      // Connect
+      console.log('Connecting to ' + host + ':' + port + '...');
+      this.MakeConnection();
    }
 }
-
-function endEventHandler() {
-   console.log('end');
-}
-
-function timeoutEventHandler() {
-   console.log('timeout');
-}
-
-function drainEventHandler() {
-   // console.log('drain');
-}
-
-function errorEventHandler() {
-   console.log('error');
-}
-
-function closeEventHandler () {
-   console.log('close');
-   if (!retrying) {
-       retrying = true;
-       console.log('Reconnecting...');
-   }
-   setTimeout(makeConnection, timeout);
-}
-
-// Create socket and bind callbacks
-let socket = new net.Socket();
-socket.on('connect', connectEventHandler);
-socket.on('data',    dataEventHandler);
-socket.on('end',     endEventHandler);
-socket.on('timeout', timeoutEventHandler);
-socket.on('drain',   drainEventHandler);
-socket.on('error',   errorEventHandler);
-socket.on('close',   closeEventHandler);
-
-// Connect
-console.log('Connecting to ' + host + ':' + port + '...');
-makeConnection();
