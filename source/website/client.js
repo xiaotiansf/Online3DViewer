3b875c73a47932e35d43cbcc76405c4cde847d7f
xiaos <xiaos@xiaos-clx-desktop>
2024-01-05 11:24:22 -0800

    parse 3D model cmd payload.

--- source/website/client.js
+++ source/website/client.js
@@ -5,7 +5,22 @@ var client = net.connect({port: 9838}, function() {
 
 client.on('data', function(data) {
    console.log(data.toString());
-   client.end();
+   let cmd_string = data.toString();
+   let hashtag_index = cmd_string.indexOf('#{');
+   if (hashtag_index > 0) {
+      let json = cmd_string.slice(hashtag_index+1);
+      const obj = JSON.parse(json);
+      console.log(obj.filename);
+      console.log(obj.info);
+      console.log(obj.direction);
+      console.log(obj.device_orientation);
+      console.log(obj.display_mode);
+      console.log(obj.zoom);
+      console.log(obj.assetDir);
+   }
+   else {
+      console.log('received invalid string from palacio-display-server');
+   }
 });
 
 client.on('end', function() {
