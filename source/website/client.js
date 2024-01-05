d66733105647e729cafb0f6f437f15b5c084dc08
xiaos <xiaos@xiaos-clx-desktop>
2024-01-05 14:34:15 -0800

    update for reconnection on connection timeout.

--- source/website/client.js
+++ source/website/client.js
@@ -1,9 +1,20 @@
 import net from 'net'
-var client = net.connect({port: 9838}, function() {
-   console.log('connected to palacio-display-server!');
-});
+const port = 9838;
+const host = '127.0.0.1';
+const timeout = 5000;
+let retrying = false;
 
-client.on('data', function(data) {
+// Functions to handle socket events
+function makeConnection () {
+   socket.connect(port, host);
+}
+
+function connectEventHandler() {
+   console.log('connected');
+   retrying = false;
+}
+
+function dataEventHandler() {
    console.log(data.toString());
    let cmd_string = data.toString();
    let hashtag_index = cmd_string.indexOf('#{');
@@ -21,8 +32,43 @@ client.on('data', function(data) {
    else {
       console.log('received invalid string from palacio-display-server');
    }
-});
+}
 
-client.on('end', function() {
-   console.log('disconnected from palacio-display-server');
-});
+function endEventHandler() {
+   console.log('end');
+}
+
+function timeoutEventHandler() {
+   console.log('timeout');
+}
+
+function drainEventHandler() {
+   // console.log('drain');
+}
+
+function errorEventHandler() {
+   console.log('error');
+}
+
+function closeEventHandler () {
+   console.log('close');
+   if (!retrying) {
+       retrying = true;
+       console.log('Reconnecting...');
+   }
+   setTimeout(makeConnection, timeout);
+}
+
+// Create socket and bind callbacks
+let socket = new net.Socket();
+socket.on('connect', connectEventHandler);
+socket.on('data',    dataEventHandler);
+socket.on('end',     endEventHandler);
+socket.on('timeout', timeoutEventHandler);
+socket.on('drain',   drainEventHandler);
+socket.on('error',   errorEventHandler);
+socket.on('close',   closeEventHandler);
+
+// Connect
+console.log('Connecting to ' + host + ':' + port + '...');
+makeConnection();
